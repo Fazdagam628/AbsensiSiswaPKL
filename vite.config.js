37599e108cbbb25b018876bc2b3630ec87edb0f7
Fazdagam628 <fauzanmasud628@gmail.com>
2025-04-23 17:54:51 +0700

    add

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            // refresh: true,
-             refresh: [
-                ...refreshPaths,
-                'app/Livewire/**',
-            ],
+            refresh: true,
+            //  refresh: [
+            //     ...refreshPaths,
+            //     'app/Livewire/**',
+            // ],
         }),
         tailwindcss(),
     ],
