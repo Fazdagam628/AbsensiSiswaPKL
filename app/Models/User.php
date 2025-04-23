ed6ed2cb2c11c39f78da6e0e8de23f6ab793386a
Fazdagam628 <fauzanmasud628@gmail.com>
2025-04-23 20:23:17 +0700

    add

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,8 @@ use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 
 
-class User extends Authenticatable
+
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasRoles;
